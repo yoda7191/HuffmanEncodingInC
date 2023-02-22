2df567a385bc89901b468a84f2b38b9ef0929474
yoda7191 <yoda7191@gmail.com>
2023-02-22 04:17:04 +0100

    tree skeleton

--- src/calculatefrequencies.h
+++ src/calculatefrequencies.h
@@ -3,6 +3,13 @@
 
 #include "stdio.h"
 
+typedef struct treeNode_t
+{
+    struct treeNode_t *left, *right;
+    int freq;
+    char c;
+} *node;
+
 void calculateFrequencies(FILE *readFile, int *freqArrayPointer[128]);
 
 #endif  
